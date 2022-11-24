15aa5685840a4008afd4c0c8393943d1747b156b
kifahnaseer <kifahnaeer28@gmail.com>
2022-11-24 23:23:25 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code in this file!
+const currentUser = 'Kifah Abdinasir';
+const welcomeMessage = 'Welcome to Flatbook, Kifah Abdinasir!';
+const excitedWelcomeMessage = "WELCOME TO FLATBOOK, KIFAH ABDINASIR!";
+const shortGreeting = "Welcome, K!";
